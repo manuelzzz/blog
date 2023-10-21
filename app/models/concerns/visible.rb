183066033cb4ec3a3b9e64bf51e14baf6077c5ee
Manel <manuelss.23.bil@gmail.com>
2023-10-21 17:16:15 -0300

    Statuses of visible comments and articles in creation

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -3,11 +3,11 @@ module Visible
 
   VALID_STATUSES = ['public', 'private', 'archived']
 
-  include do
+  included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
-  class_methdo do
+  class_methods do
     def public_count
       where(status: 'public').count
     end
